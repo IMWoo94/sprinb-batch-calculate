baf7b6d3d51094cca14119c6265c9eb4a0d3fb3a
IMWoo94 <leemeawoo94@gmail.com>
2024-02-27 21:26:33 +0900

    feat : email 전송 Fake 객체 생성
    1. EmailProvider interface 생성
    2. EmailPRovider 구현체 Fake 구현
    3. 이메일 전송 시 log 출력

---
+++ src/main/java/com/lsm/batch/customer/EmailProvider.java
@@ -0,0 +1,18 @@
+package com.lsm.batch.customer;
+
+import lombok.extern.slf4j.Slf4j;
+
+// 실제 이메일을 전송 하지 않고 log 만 작성
+public interface EmailProvider {
+
+	void send(String emailAddress, String title, String body);
+
+	@Slf4j
+	class Fake implements EmailProvider{
+
+		@Override
+		public void send(String emailAddress, String title, String body) {
+			log.info("{} email 전송 완료! {} :  {}", emailAddress, title, body);
+		}
+	}
+}
