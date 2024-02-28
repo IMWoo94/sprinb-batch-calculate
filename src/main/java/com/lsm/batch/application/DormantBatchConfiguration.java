dd404d8c8cf6a1136bc0fedea44ffb2f7187113a
IMWoo94 <leemeawoo94@gmail.com>
2024-02-28 18:15:45 +0900

    refactor : 비즈니스 로직 세분화 설정

--- src/main/java/com/lsm/batch/application/DormantBatchConfiguration.java
+++ src/main/java/com/lsm/batch/application/DormantBatchConfiguration.java
@@ -3,18 +3,25 @@ package com.lsm.batch.application;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.lsm.batch.customer.Customer;
 import com.lsm.batch.dormantbatch.Job;
+import com.lsm.batch.dormantbatch.SimpleTasklet;
 
 @Configuration
 public class DormantBatchConfiguration {
 
 	@Bean
 	public Job dormantBatchJob(
-		DormantBatchTasklet dormantBatchTasklet,
+		DormantBatchItemReader dormantBatchItemReader,
+		DormantBatchItemProcessor dormantBatchItemProcessor,
+		DormantBatchItemWriter dormantBatchItemWriter,
 		DormantBatchJobExecutionListener dormantBatchJobExecutionListener
 	) {
+		SimpleTasklet<Customer, Customer> tasklet = new SimpleTasklet<>(dormantBatchItemReader,
+			dormantBatchItemProcessor, dormantBatchItemWriter);
+		
 		return new Job(
-			dormantBatchTasklet,
+			tasklet,
 			dormantBatchJobExecutionListener
 		);
 	}
