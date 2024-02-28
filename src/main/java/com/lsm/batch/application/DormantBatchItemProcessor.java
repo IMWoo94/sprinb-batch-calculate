7bfc59f0c178f4bdeecca2c8bb4fe570aa6d2564
IMWoo94 <leemeawoo94@gmail.com>
2024-02-28 18:15:11 +0900

    refactor : 비즈니스 로직 세분화 구현
    1. Reader - DormantBatchItemReader
    2. Processor - DormantBatchItemProcessor
    3. Writer - DormantBathItemWriter

---
+++ src/main/java/com/lsm/batch/application/DormantBatchItemProcessor.java
@@ -0,0 +1,28 @@
+package com.lsm.batch.application;
+
+import java.time.LocalDate;
+
+import org.springframework.stereotype.Component;
+
+import com.lsm.batch.customer.Customer;
+import com.lsm.batch.dormantbatch.ItemProcessor;
+
+@Component
+public class DormantBatchItemProcessor implements ItemProcessor<Customer, Customer> {
+
+	@Override
+	public Customer process(Customer customer) {
+		// 2. 휴면계정 대상을 추출 및 변환한다.
+		// 로그인 날짜 	/		365일 전			/ 		오늘
+		final boolean isDormantTarget = LocalDate.now()
+			.minusDays(365)
+			.isAfter(customer.getLoginAt().toLocalDate());
+
+		if (isDormantTarget) {
+			customer.setStatus(Customer.Status.DORMANT);
+			return customer;
+		} else {
+			return null;
+		}
+	}
+}
---
+++ src/main/java/com/lsm/batch/application/DormantBatchItemReader.java
@@ -0,0 +1,37 @@
+package com.lsm.batch.application;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Component;
+
+import com.lsm.batch.customer.Customer;
+import com.lsm.batch.customer.CustomerRepository;
+import com.lsm.batch.dormantbatch.ItemReader;
+
+@Component
+public class DormantBatchItemReader implements ItemReader<Customer> {
+
+	private int pageNo = 0;
+	private final CustomerRepository customerRepository;
+
+	public DormantBatchItemReader(CustomerRepository customerRepository) {
+		this.customerRepository = customerRepository;
+	}
+
+	@Override
+	public Customer read() {
+		// 1. 고객 조회
+		// 한명의 고객씩 가져오기
+		final PageRequest pageRequest = PageRequest.of(pageNo, 1, Sort.by("id").ascending());
+		final Page<Customer> page = customerRepository.findAll(pageRequest);
+
+		if (page.isEmpty()) {
+			pageNo = 0;
+			return null;
+		} else {
+			pageNo++;
+			return page.getContent().get(0);
+		}
+	}
+}
