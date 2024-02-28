8efcf2a1920cf7a4e40c4ba3f0ee64dcdb2e222d
IMWoo94 <leemeawoo94@gmail.com>
2024-02-28 16:14:48 +0900

    feat : 배치 실행 시 작업 정보 등록 및 리턴
    1. 배치 동작 시 작업에 대한 정보를 JobExecution 에 담아서 리턴

--- src/main/java/com/lsm/batch/DormantBatchJob.java
+++ src/main/java/com/lsm/batch/DormantBatchJob.java
@@ -1,6 +1,7 @@
 package com.lsm.batch;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -9,6 +10,8 @@ import org.springframework.stereotype.Component;
 
 import com.lsm.batch.customer.Customer;
 import com.lsm.batch.customer.CustomerRepository;
+import com.lsm.batch.dormantbatch.BatchStatus;
+import com.lsm.batch.dormantbatch.JobExecution;
 
 @Component
 public class DormantBatchJob {
@@ -21,41 +24,55 @@ public class DormantBatchJob {
 		this.emailProvider = new EmailProvider.Fake();
 	}
 
-	public void execute() {
+	public JobExecution execute() {
+
+		JobExecution jobExecution = new JobExecution();
+		jobExecution.setStatus(BatchStatus.STARTING);
+		jobExecution.setStartTime(LocalDateTime.now());
+
 		int pageNo = 0;
 
-		while (true) {
-			// 1. 고객 조회
-			// 한명의 고객씩 가져오기
-			final PageRequest pageRequest = PageRequest.of(pageNo, 1, Sort.by("id").ascending());
-			final Page<Customer> page = customerRepository.findAll(pageRequest);
+		try {
+			while (true) {
+				// 1. 고객 조회
+				// 한명의 고객씩 가져오기
+				final PageRequest pageRequest = PageRequest.of(pageNo, 1, Sort.by("id").ascending());
+				final Page<Customer> page = customerRepository.findAll(pageRequest);
 
-			final Customer customer;
-			if (page.isEmpty()) {
-				break;
-			} else {
-				pageNo++;
-				customer = page.getContent().get(0);
+				final Customer customer;
+				if (page.isEmpty()) {
+					break;
+				} else {
+					pageNo++;
+					customer = page.getContent().get(0);
+				}
+
+				// 2. 휴면계정 대상을 추출 및 변환한다.
+				// 로그인 날짜 	/		365일 전			/ 		오늘
+				final boolean isDormantTarget = LocalDate.now()
+					.minusDays(365)
+					.isAfter(customer.getLoginAt().toLocalDate());
+
+				if (isDormantTarget) {
+					customer.setStatus(Customer.Status.DORMANT);
+				} else {
+					continue;
+				}
+
+				// 3. 휴면계정으로 상태를 변경한다.
+				customerRepository.save(customer);
+
+				// 4. 메일을 보낸다.
+				emailProvider.send(customer.getEmail(), "휴면전환 안내메일입니다.", "휴면 전환");
 			}
-
-			// 2. 휴면계정 대상을 추출 및 변환한다.
-			// 로그인 날짜 	/		365일 전			/ 		오늘
-			final boolean isDormantTarget = LocalDate.now()
-				.minusDays(365)
-				.isAfter(customer.getLoginAt().toLocalDate());
-
-			if (isDormantTarget) {
-				customer.setStatus(Customer.Status.DORMANT);
-			} else {
-				continue;
-			}
-
-			// 3. 휴면계정으로 상태를 변경한다.
-			customerRepository.save(customer);
-
-			// 4. 메일을 보낸다.
-			emailProvider.send(customer.getEmail(), "휴면전환 안내메일입니다.", "휴면 전환");
+			jobExecution.setStatus(BatchStatus.COMPLETED);
+		} catch (Exception e) {
+			jobExecution.setStatus(BatchStatus.FAILED);
 		}
+
+		jobExecution.setEndTime(LocalDateTime.now());
+
+		return jobExecution;
 
 	}
 }
