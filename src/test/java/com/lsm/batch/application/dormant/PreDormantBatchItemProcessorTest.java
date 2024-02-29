d06041a00da5995c01c5f1ac4d88b532a3a32a45
IMWoo94 <leemeawoo94@gmail.com>
2024-02-29 15:03:16 +0900

    test : 휴면 전환 고지 안내 프로세스 테스트 작성
    로그인 1주일 이전에 휴면 전환 대상 고지 안내 프로세스 테스트 케이스 작성
    1. 로그인 날짜가 오늘로부터 358일 전이면 customer 반환
    2. 로그인 날짜가 오늘로부터 358일 전이 아니면 null 반환

---
+++ src/test/java/com/lsm/batch/application/dormant/PreDormantBatchItemProcessorTest.java
@@ -0,0 +1,54 @@
+package com.lsm.batch.application.dormant;
+
+import java.time.LocalDateTime;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.lsm.batch.customer.Customer;
+
+class PreDormantBatchItemProcessorTest {
+
+	private PreDormantBatchItemProcessor preDormantBatchItemProcessor;
+
+	@BeforeEach
+	void setup() {
+		preDormantBatchItemProcessor = new PreDormantBatchItemProcessor();
+	}
+
+	@Test
+	@DisplayName("로그인 날짜가 오늘로부터 358일 전이면 customer 를 반환해야 한다.")
+	void test1() {
+
+		// given
+		Customer customer = new Customer("lee", "lee@test.com");
+
+		customer.setLoginAt(LocalDateTime.now().minusDays(358));
+
+		// when
+		Customer result = preDormantBatchItemProcessor.process(customer);
+
+		// then
+		Assertions.assertThat(result).isEqualTo(customer);
+		Assertions.assertThat(result).isNotNull();
+	}
+
+	@Test
+	@DisplayName("로그인 날짜가 오늘로부터 358일 전이 아니면 null을 반환해야 한다.")
+	void test2() {
+
+		// given
+		Customer customer = new Customer("lee", "lee@test.com");
+
+		customer.setLoginAt(LocalDateTime.now());
+
+		// when
+		Customer result = preDormantBatchItemProcessor.process(customer);
+
+		// then
+		Assertions.assertThat(result).isNull();
+	}
+
+}
