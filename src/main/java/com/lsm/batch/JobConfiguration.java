951be425837412204a2e489afe54e3a8bb6ee799
IMWoo94 <leemeawoo94@gmail.com>
2024-03-01 17:57:03 +0900

    feat : Spring Batch Step Skip 옵션 구성
    1. 배치 스킵 처리를 위해 faultTolerant() 옵션으로 활성화
    2. IllegalStateException 예외에 대해서 3회 까지는 Skip 처리

--- src/main/java/com/lsm/batch/JobConfiguration.java
+++ src/main/java/com/lsm/batch/JobConfiguration.java
@@ -5,7 +5,10 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.NonTransientResourceException;
+import org.springframework.batch.item.ParseException;
+import org.springframework.batch.item.UnexpectedInputException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -25,46 +28,37 @@ public class JobConfiguration {
 
 	@Bean
 	public Step step(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
+		ItemReader<Integer> itemReader = new ItemReader<>() {
+
+			private int count = 0;
+
+			@Override
+			public Integer read() throws
+				Exception,
+				UnexpectedInputException,
+				ParseException,
+				NonTransientResourceException {
+				count++;
+
+				log.info("Read {}", count);
+
+				if (count >= 15) {
+					throw new IllegalStateException("예외 발생" + count);
+				}
+
+				return count;
+			}
+		};
+
 		return new StepBuilder("step", jobRepository)
-			.tasklet((contribution, chunkContext) -> {
-				log.info("step 실행 {} , {}", contribution, chunkContext);
-				return RepeatStatus.FINISHED;
-			}, platformTransactionManager)
-			.allowStartIfComplete(true) // 재시작 가능
-			.startLimit(5)
+			.chunk(10, platformTransactionManager)
+			.reader(itemReader)
+			// .processor()
+			.writer(read -> {
+			})
+			.faultTolerant()
+			.skip(IllegalStateException.class)
+			.skipLimit(3)
 			.build();
 	}
-
-	// @Bean
-	// public Step step(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
-	// 	ItemReader<Integer> itemReader = new ItemReader<>() {
-	//
-	// 		private int count = 0;
-	//
-	// 		@Override
-	// 		public Integer read() throws
-	// 			Exception,
-	// 			UnexpectedInputException,
-	// 			ParseException,
-	// 			NonTransientResourceException {
-	// 			count++;
-	//
-	// 			log.info("Read {}", count);
-	//
-	// 			if (count == 15) {
-	// 				return null;
-	// 			}
-	//
-	// 			return count;
-	// 		}
-	// 	};
-	//
-	// 	return new StepBuilder("step", jobRepository)
-	// 		.chunk(10, platformTransactionManager)
-	// 		.reader(itemReader)
-	// 		// .processor()
-	// 		.writer(read -> {
-	// 		})
-	// 		.build();
-	// }
 }
