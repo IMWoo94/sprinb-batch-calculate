fa7241cd1324a834a17fbb021e96fb96d21866fd
IMWoo94 <leemeawoo94@gmail.com>
2024-03-04 20:37:14 +0900

    feat : 정산 프로그램 구현 Init
    1. 불필요 파일 제거

--- src/main/java/com/lsm/batch/entity/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.lsm.batch.entity;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.lsm.batch.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
