1bd4d21f399d302f62635f4b3baa39af6dde2ce4
IMWoo94 <leemeawoo94@gmail.com>
2024-03-08 19:31:29 +0900

    refactor : Spring Batch 개선 스케줄러
    1. Spring 스케줄러 동작 활성화 처리

--- src/main/java/com/lsm/batch/BatchApplication.java
+++ src/main/java/com/lsm/batch/BatchApplication.java
@@ -2,9 +2,10 @@ package com.lsm.batch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-
+@EnableScheduling
 public class BatchApplication {
 
 	public static void main(String[] args) {
