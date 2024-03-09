9d28d0cd39039e94d1e359eba90a8a0e4a1b025e
IMWoo94 <leemeawoo94@gmail.com>
2024-03-09 19:02:54 +0900

    refactor : 정산 배치 작업 Quartz Job 으로 탈바꿈
    1. Quartz Job 으로 선언하고 비즈니스 로직 작성
    - Spring batch 로 작성한 정산 배치 프로그램 동작

---
+++ src/main/java/com/lsm/batch/calculateBatch/scheduler/quartz/SettleQuartzJob.java
@@ -0,0 +1,44 @@
+package com.lsm.batch.calculateBatch.scheduler.quartz;
+
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class SettleQuartzJob extends QuartzJobBean {
+
+	private final JobLauncher jobLauncher;
+	private final Job settleJob;
+
+	public SettleQuartzJob(JobLauncher jobLauncher, Job settleJob) {
+		this.jobLauncher = jobLauncher;
+		this.settleJob = settleJob;
+	}
+
+	@Override
+	protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+		JobParameters parameters = new JobParametersBuilder()
+			.addString("targetDate", "20240225")
+			.addString("totalCount", "500000")
+			.toJobParameters();
+
+		try {
+			jobLauncher.run(settleJob, parameters);
+		} catch (JobExecutionAlreadyRunningException | JobInstanceAlreadyCompleteException | JobRestartException |
+				 JobParametersInvalidException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
