adeb964c57f1c98bb6114e6e1abf00ff86b7165d
IMWoo94 <leemeawoo94@gmail.com>
2024-03-09 15:58:55 +0900

    test : Spring Scheduling Thread pool issue
    1. Spring Scheduling Thread Pool size 이슈 테스트

--- src/main/java/com/lsm/batch/calculateBatch/runner/JobRunnerConfiguration.java
+++ src/main/java/com/lsm/batch/calculateBatch/runner/JobRunnerConfiguration.java
@@ -1,6 +1,10 @@
 package com.lsm.batch.calculateBatch.runner;
 
+import java.time.LocalDateTime;
+
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
@@ -21,21 +25,39 @@ public class JobRunnerConfiguration {
 	private final Job settleJob;
 	private int count;
 
-	// 20초 후 반복
-	@Scheduled(fixedDelay = 20000L)
+	// 20 초 마다 정산 배치 프로세스
+	// @Scheduled(fixedDelay = 20000L)
 	public void settleJobRun() throws
 		JobInstanceAlreadyCompleteException,
 		JobExecutionAlreadyRunningException,
 		JobParametersInvalidException,
 		JobRestartException {
-		log.info("@Scheduled 20초 마다 반복 {}", count++);
-		// JobParameters parameters = new JobParametersBuilder()
-		// 	.addString("targetDate", "20240225")
-		// 	.addString("totalCount", "500000")
-		// 	.toJobParameters();
-		//
-		// jobLauncher.run(settleJob, parameters);
+		JobParameters parameters = new JobParametersBuilder()
+			.addString("targetDate", "20240225")
+			.addString("totalCount", "500000")
+			.toJobParameters();
 
+		jobLauncher.run(settleJob, parameters);
+	}
+
+	@Scheduled(fixedRate = 3000)
+	public void fixedRate1() throws InterruptedException {
+		LocalDateTime start = LocalDateTime.now();
+		log.info(">>>>>fixedRate1 시작 시간 {}", start);
+		Thread.sleep(5000);
+		LocalDateTime end = LocalDateTime.now();
+		int total = end.getSecond() - start.getSecond();
+		log.info(">>>>>fixedRate1 종료 시간 {} , 총 소요 시간 : {}", end, total);
+	}
+
+	@Scheduled(fixedRate = 3000)
+	public void fixedRate2() throws InterruptedException {
+		LocalDateTime start = LocalDateTime.now();
+		log.info(">>>>>fixedRate2 시작 시간 {}", start);
+		Thread.sleep(5000);
+		LocalDateTime end = LocalDateTime.now();
+		int total = end.getSecond() - start.getSecond();
+		log.info(">>>>>fixedRate2 종료 시간 {} , 총 소요 시간 : {}", end, total);
 	}
 
 }
